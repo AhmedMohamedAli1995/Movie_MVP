89b11e61c5aaff45ee937c5d022149676678eac5
Mac <home@SW18-ahmedali-mac.local>
2020-02-24 16:34:19 +0200

    add simple network layer

---
+++ MovieApp/Service/NetworkLayer.swift
@@ -0,0 +1,13 @@
+//
+//  APIService.swift
+//  MovieApp
+//
+//  Created by Mac  on 2/24/20.
+//  Copyright © 2020 com.megatrustgroup.asli. All rights reserved.
+//
+
+import UIKit
+
+class APIService: NSObject {
+
+}
