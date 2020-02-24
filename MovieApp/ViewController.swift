42c7e94236fbcd18f7012e82e8fb7290306c9f8a
Mac <home@SW18-ahmedali-mac.local>
2020-02-24 10:44:12 +0200

    git commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mac  on 2/24/20.
+//  Copyright © 2020 com.megatrustgroup.asli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
